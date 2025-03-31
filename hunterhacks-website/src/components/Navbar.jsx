09a9f86ddfa27589970702628e223322d565342c
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2025-03-30 22:27:17 -0400

    sidebar navigation for mobile

--- hunterhacks-website/src/components/Navbar.jsx
+++ hunterhacks-website/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import './Navbar.css'
+import HambergerIcon from '../assets/menu.svg'
+import {useRef} from 'react';
 
 function Navbar({
     aboutRef, sponsorRef, judgeRef, scheduleRef, 
@@ -6,37 +8,87 @@ function Navbar({
     scrollFunction
 })
 {
-    return(
-        <div id="Navbar">
-            <p>
-                HunterHacks    
-            </p>
+    const sidebarRef = useRef(null); 
+    const sidebarControl = (sidebarRef, sectionRef) => {
+        sidebarRef.current?.classList.toggle("reveal");
+        console.log("PLEASE")
 
-            <nav style={{display:"flex",gap:"1rem"}}>
-                <a onClick={() => scrollFunction(aboutRef)}>
+        if( sectionRef)
+        {
+            scrollFunction(sectionRef);
+        }
+    };
+
+    return(
+        <>
+            <div id="Navbar">
+                <p>
+                    HunterHacks    
+                </p>
+
+                <nav id="Nav">
+                    <a onClick={() => scrollFunction(aboutRef)}>
+                        About
+                    </a>
+                    <a onClick={() => scrollFunction(sponsorRef)}>
+                        Sponsors
+                    </a>
+                    <a onClick={() => scrollFunction(judgeRef)}>
+                        Judges
+                    </a>
+                    <a onClick={() => scrollFunction(scheduleRef)}>
+                        Schedule
+                    </a>
+                    <a onClick={() => scrollFunction(trackRef)}>
+                        Tracks
+                    </a>
+                    <a onClick={() => scrollFunction(applicationRef)}>
+                        Apply!
+                    </a>
+                    <a onClick={() => scrollFunction(faqRef)}>
+                        FAQ
+                    </a>
+                    
+                </nav>
+                <a id="Hamburger">
+                    <img src={HambergerIcon} alt="Hamburger" onClick={()=>sidebarControl(sidebarRef)}></img>
+                </a>
+            </div>
+
+            <nav ref={sidebarRef} className='Sidebar'>
+                <a onClick={() => sidebarControl(sidebarRef,aboutRef)}>
                     About
                 </a>
-                <a onClick={() => scrollFunction(sponsorRef)}>
+                <a onClick={() => sidebarControl(sidebarRef,sponsorRef)}>
                     Sponsors
                 </a>
-                <a onClick={() => scrollFunction(judgeRef)}>
+                <a onClick={() => sidebarControl(sidebarRef, judgeRef)}>
                     Judges
                 </a>
-                <a onClick={() => scrollFunction(scheduleRef)}>
-                    scheduleRef
+                <a onClick={() => sidebarControl(sidebarRef, scheduleRef)}>
+                    Schedule
                 </a>
-                <a onClick={() => scrollFunction(trackRef)}>
+                <a onClick={() => sidebarControl(sidebarRef, trackRef)}>
                     Tracks
                 </a>
-                <a onClick={() => scrollFunction(applicationRef)}>
+                <a onClick={() => sidebarControl(sidebarRef, applicationRef)}>
                     Apply!
                 </a>
-                <a onClick={() => scrollFunction(faqRef)}>
+                <a onClick={() => sidebarControl(sidebarRef, faqRef)}>
                     FAQ
                 </a>
-          
                 
             </nav>
-        </div>
+        </>
     );
 }export default Navbar
+
+// function Sidebar({
+//     aboutRef, sponsorRef, judgeRef, scheduleRef, 
+//     trackRef, applicationRef, faqRef,
+//     scrollFunction
+// }){
+//     return(
+        
+//     );
+// }
