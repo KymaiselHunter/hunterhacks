7d00f696b40fb49f95b63cea8b3965e0f24bb3af
ynaloisp <ynaloispangilinan@gmail.com>
2025-04-18 00:42:28 -0400

    update schedule list

--- hunterhacks-website/src/components/ScheduleList.jsx
+++ hunterhacks-website/src/components/ScheduleList.jsx
@@ -1,87 +1,83 @@
 import ScheduleDay from "./ScheduleDay";
-import "./ScheduleList.css"
+import "./ScheduleList.css";
 
 const activitiesFriday = [
-    {
-        activityTitle: "Check-in Begins",
-        activityDescription: "1PM – Hunter West Lobby",
-    },
-    {
-        activityTitle: "Opening Ceremony",
-        activityDescription: "2PM – Hunter West Auditorium",
-    },
-    {
-        activityTitle: "MLT Introduction",
-        activityDescription: "2:30PM – Hunter West Auditorium",
-    },
-    {
-        activityTitle: "Check-in Ends, Team Registration + Mentor Matching",
-        activityDescription: "3PM – Hunter West Auditorium → Hunter East Library",
-    },
-    {
-        activityTitle: "In-Person Hacking Ends",
-        activityDescription: "9PM – Hunter East Library",
-    },
+  {
+    activityTitle: "Check-in Begins",
+    activityDescription: "1PM – Hunter West Lobby",
+  },
+  {
+    activityTitle: "Opening Ceremony",
+    activityDescription: "2PM – Hunter West Lobby",
+  },
+  {
+    activityTitle: "MLT Introduction",
+    activityDescription: "2:30PM – Hunter West Auditorium",
+  },
+  {
+    activityTitle: "Check-in Ends, Team Registration + Mentor Matching",
+    activityDescription: "3PM – Hunter West Auditorium → Hunter East Library",
+  },
+  {
+    activityTitle: "In-Person Hacking Ends",
+    activityDescription: "9PM – Hunter East Library",
+  },
 ];
 const activitiesSaturday = [
-    {
-        activityTitle: "Check-in Begins",
-        activityDescription: "1PM – Hunter West Lobby",
-    },
-    {
-        activityTitle: "Opening Ceremony",
-        activityDescription: "2PM – Hunter West Auditorium",
-    },
-    {
-        activityTitle: "MLT Introduction",
-        activityDescription: "2:30PM – Hunter West Auditorium",
-    },
-    {
-        activityTitle: "Check-in Ends, Team Registration + Mentor Matching",
-        activityDescription: "3PM – Hunter West Auditorium → Hunter East Library",
-    },
-    {
-        activityTitle: "In-Person Hacking Ends",
-        activityDescription: "9PM – Hunter East Library",
-    },
+  {
+    activityTitle: "Breakfast",
+    activityDescription: "10AM – Hunter West Cafeteria",
+  },
+  {
+    activityTitle: "Lunch",
+    activityDescription: "1PM – Hunter West Cafeteria",
+  },
+  {
+    activityTitle: "MLT Introduction",
+    activityDescription: "2:30PM – Hunter West Auditorium",
+  },
+  {
+    activityTitle: "Check-in Ends, Team Registration + Mentor Matching",
+    activityDescription: "3PM – Hunter West Auditorium → Hunter East Library",
+  },
+  {
+    activityTitle: "In-Person Hacking Ends",
+    activityDescription: "9PM – Hunter East Library",
+  },
 ];
 const activitiesSunday = [
-    {
-        activityTitle: "Check-in Begins",
-        activityDescription: "1PM – Hunter West Lobby",
-    },
-    {
-        activityTitle: "Opening Ceremony",
-        activityDescription: "2PM – Hunter West Auditorium",
-    },
-    {
-        activityTitle: "MLT Introduction",
-        activityDescription: "2:30PM – Hunter West Auditorium",
-    },
-    {
-        activityTitle: "Check-in Ends, Team Registration + Mentor Matching",
-        activityDescription: "3PM – Hunter West Auditorium → Hunter East Library",
-    },
-    {
-        activityTitle: "In-Person Hacking Ends",
-        activityDescription: "9PM – Hunter East Library",
-    },
+  {
+    activityTitle: "Breakfast",
+    activityDescription: "10AM – Hunter West Cafeteria",
+  },
+  {
+    activityTitle: "Hacking Ends, Lunch Begins",
+    activityDescription: "12PM – Hunter West Cafeteria",
+  },
+  {
+    activityTitle: "Judging Begins",
+    activityDescription: "1PM – Hunter West Cafeteria",
+  },
+  {
+    activityTitle: "Judging Ends",
+    activityDescription: "3PM – Hunter West Cafeteria",
+  },
+  {
+    activityTitle: "Closing Ceremony",
+    activityDescription: "4PM - Hunter West 714",
+  },
 ];
 
-
-function ScheduleList()
-{
-    return(
-        <div className="schedule-list">
-            <ScheduleDay
-                activityArray={activitiesFriday}
-            ></ScheduleDay>
-            <ScheduleDay
-                activityArray={activitiesSaturday}
-            ></ScheduleDay>
-            <ScheduleDay
-                activityArray={activitiesSunday}
-            ></ScheduleDay>
-        </div>
-    );
-}export default ScheduleList
+function ScheduleList() {
+  return (
+    <div className="schedule-list">
+      <ScheduleDay activityArray={activitiesFriday} day="Friday"></ScheduleDay>
+      <ScheduleDay
+        activityArray={activitiesSaturday}
+        day="Saturday"
+      ></ScheduleDay>
+      <ScheduleDay activityArray={activitiesSunday} day="Sunday"></ScheduleDay>
+    </div>
+  );
+}
+export default ScheduleList;
